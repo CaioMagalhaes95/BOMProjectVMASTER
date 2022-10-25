a655f051f0dbc44b6fa10f97c91a3b001bae9b39
teo.mendonca1510@gmail.com <teo.mendonca1510@gmail.com>
2022-10-24 21:50:25 -0300

    Search Worcking

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -18,81 +18,68 @@ router.use(bodyParser.urlencoded({ extended: true }))
 
 //material routes
 
-router.post('/material', (req,res) => {
-    const {nome, descricao, datac, quantidade, sistema, massa, custo, unidadedemedida, composicao} = req.body
+router.post('/material', (req, res) => {
+    const { nome, descricao, datac, quantidade, sistema, massa, custo, unidadedemedida, composicao } = req.body
 
-   
     sequelize.query(`INSERT INTO Materiais (nome, descricao, datac, quantidade, sistema, massa, custo, unidadedemedida, composicao) values (?, ?, ?, ?, ?, ?, ?, ?, ?)`, {
-        
+
         type: QueryTypes.INSERT,
         replacements: [nome, descricao, datac, quantidade, sistema, massa, custo, unidadedemedida, composicao],
-            
-       
+
     })
-    .then(() => res.end('done'))
-    .catch(err => res.end(err));
-    
-
-    
-
+        .then(() => res.end('done'))
+        .catch(err => res.end(err));
 })
 
 
-router.get('/material/:nome', (req,res) =>{
-    const nomeSearch = req.params.nome   
-    
-    if(!nomeSearch){
-        return res.status(404).json({msg: 'Usuario não encontrado'})
+router.get('/material/:nome', (req, res) => {
+    const nomeSearch = req.params.nome
+
+    if (!nomeSearch) {
+        return res.status(404).json({ msg: 'Usuario não encontrado' })
     }
     sequelize.query(`SELECT * FROM Materiais where nome = ?`, nomeSearch, {
-        
+
         type: QueryTypes.SELECT,
         replacements: nomeSearch
 
-
-    }).then(()=> res.end())
-    .catch(err => res.end(err))
+    }).then(() => res.end())
+        .catch(err => res.end(err))
 })
 
 
-router.post('/cars', (req,res) => {
-    const {nome, descricao} = req.body
+router.post('/cars', (req, res) => {
+    const { nome, descricao } = req.body
 
-   
     sequelize.query(`INSERT INTO Carros (nomeCarro, descricaoCarro) values (?, ?)`, {
-        
+
         type: QueryTypes.INSERT,
         replacements: [nome, descricao],
-            
-       
     })
-    .then(() => res.end('done'))
-    .catch(err => res.end(err));
-    
-
-    
-
+        .then(() => res.end('done'))
+        .catch(err => res.end(err));
 })
 
-router.post('/storage', (req,res) => {
-    const {shelf, tom} = req.body
+router.get('/cars', (req, res)=> {
+    
+    sequelize.query(`Select * from Carros where nomeCarro like ?`,  {
+        type: QueryTypes.Select,
+        replacements: ['%' + req.query.search + '%']
+    }).then(cars => {
+        res.json(cars[0]);
+    })
+})
 
-   
+router.post('/storage', (req, res) => {
+    const { shelf, tom } = req.body
+
     sequelize.query(`INSERT INTO Armazem (Prateleira, tipodeproduto) values (?, ?)`, {
-        
+
         type: QueryTypes.INSERT,
         replacements: [shelf, tom]
-            
-       
     })
-    .then(() => res.end('done'))
-    .catch(err => res.end(err));
-    
-
-    
-
+        .then(() => res.end('done'))
+        .catch(err => res.end(err));
 })
-
-
 
 module.exports = router;
--- bom-front/src/pages/Search.js
+++ bom-front/src/pages/Search.js
@@ -1,39 +1,64 @@
 import './StandarStyled.css'
 import Table from 'react-bootstrap/Table'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 export default function Search(){
+  const [data, setData] = useState([])
+  const [search, setSearch] = useState("")
+
+    function getFunction(){
+      axios.get('http://localhost:3000/cars?search=' + encodeURIComponent(search))
+      .then(res => {
+        setData(res.data)
+      })
+
+    }
+
+    useEffect(()=>{
+      getFunction();
+    },[]
+    )
+
+
   return(
     
     <div className="container">
+      <input className="Nome" placeholder="Search..." value={search} onChange={(e)=> setSearch(e.target.value)}></input>
+      <button onClick={(e)=> getFunction()}>Search</button>
       <h1>Search</h1>
 
       <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Car Name</th>
+          <th>Description</th>
+          
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        {
+          data.map((car, i)=>{
+              return(
+                
+                <tr key={i}>
+                  <td>
+                    {car.IDCarro}
+                  </td>
+                  <td>
+                    {car.nomeCarro}
+                  </td>
+                  <td>
+                    {car.descricaoCarro}
+                  </td>
+                </tr>
+              )
+          })
+
+        }
+        
       </tbody>
     </Table>
     </div>
